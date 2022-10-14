f99b2b5fd7dac55d516072a6863eb3db16648d76
KarimGus <114886258+KarimGus@users.noreply.github.com>
2022-10-14 09:15:01 +0300

    Update Program.cs

--- внешние сортировки/Program.cs
+++ внешние сортировки/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.Metrics;
+using System.Diagnostics.Metrics;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Text;
@@ -6,89 +7,147 @@ using System.Text.Unicode;
 
 internal class Program
 {
-    //jjkjkj
     private static void Main(string[] args)
     {
-        using (FileStream fs1 = File.Create(Directory.GetCurrentDirectory() + "\\F1.txt"))  
-        using (FileStream fs2 = File.Create(Directory.GetCurrentDirectory() + "\\F2.txt"))
-        using (FileStream fs0 = File.Create(Directory.GetCurrentDirectory() + "\\F0.txt"))
-
-        Console.Write("введите натуральное число:"); 
-        int count = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("генерирую числа");
-
-        MakeRNDFile(count);
-        SeparateFiles(count);
-        
-
-
+        var file2 = Directory.GetCurrentDirectory() + "\\F2.txt";
+        var file1 = Directory.GetCurrentDirectory() + "\\F1.txt";
+        var file0 = Directory.GetCurrentDirectory() + "\\F0.txt";
+        using FileStream fs1 = File.Create(file1);
+        fs1.Dispose();
+        using FileStream fs2 = File.Create(file2);
+        fs2.Dispose(); 
+        //try
+        //{
+        //    Console.Write("введите число элементов:");
+        //    int count = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("введите минимальное число:");
+        //    int min = Convert.ToInt32(Console.ReadLine());
+        //    Console.Write("введите максимальное число:");
+        //    int max = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("генерирую числа");
+        //    MakeRNDFile(count, file0, min, max);
+        //}
+        //catch (InvalidCastException e)
+        //{
+        //    Console.WriteLine("вы ввели неправильный формат" + e);
+        //}    
+        MergeSort(file0, file1, file2);
     }
-    public static void MakeRNDFile(int count)
+
+     static void MakeRNDFile(int count, string file0,int min,int max)
     {
-        Random rnd = new Random();
-        using StreamWriter sr = new(Directory.GetCurrentDirectory() + "\\F0.txt");
+        Random rnd = new();
+        using (FileStream fs0 = File.Create(file0))
+            fs0.Dispose();
+        using (StreamWriter sr = new(file0))
         {
-            for (int i = 0; i < count; i++) 
-                sr.WriteLine(rnd.Next(0, 1000));                               
+            for (int i = 0; i < count; i++)
+                sr.WriteLine(rnd.Next(min, max));
+        }
+        
+    }
+
+    public static void DistributeFiles(string file0, string file1, string file2)
+    {
+        int tmp = int.MinValue;
+        int counter = 0;
+        int tmp1 = 0;
+        int tmp2 = 0;
+        string[] fs = File.ReadAllLines(file0);
+        bool fileSwitch = true;
+        using StreamWriter sw1 = new(file1);
+        using StreamWriter sw2 = new(file2);
+        while (counter < fs.Length)
+        {
+            if (fileSwitch)//СВИТЧ НУЖЕН ДЛЯ СМЕНЫ ФАЙЛА КУДА ВЕДЕТСЯ ЗАПИСЬ,то есть для смены СЕРИИ 
+            {
+                do
+                {
+                    sw1.WriteLine(fs[counter], true);
+                    tmp = Convert.ToInt32(fs[counter]);
+                    Console.WriteLine("1:" + fs[counter] + '\t' + '\t');
+                    counter++;
+                } while (counter != fs.Length && Convert.ToInt32(fs[counter]) >= tmp);
+                tmp1 = Convert.ToInt32(fs[counter]);//запсиь последнего числа в серии
+                fileSwitch = !fileSwitch;
+            }
+            else if (!fileSwitch)
+            {
+                do
+                {
+                    sw2.WriteLine(fs[counter], true);
+                    tmp = Convert.ToInt32(fs[counter]);
+                    Console.WriteLine("2:" + fs[counter] + '\t' + '\t');
+                    counter++;
+                } while (counter != fs.Length && Convert.ToInt32(fs[counter]) >= tmp);
+                tmp2 = Convert.ToInt32(fs[counter]);//запсиь последнего числа в серии
+                fileSwitch = !fileSwitch;
+            }
+            tmp = int.MinValue;          
         }
     }
-    public static void SeparateFiles(int count)
-    {
-        var pathf2 = Directory.GetCurrentDirectory() + "\\F2.txt";
-        var pathf1 = Directory.GetCurrentDirectory() + "\\F1.txt";
-        var pathf0 = Directory.GetCurrentDirectory() + "\\F0.txt";
-        int b = int.MinValue;
-        bool selectedfile = true;     
-        StreamReader stream0 = new(pathf0);
-        StreamWriter stream1 = new(pathf1);
-        StreamWriter stream2 = new(pathf2);
 
-        string line;
-        while ((line = stream0.ReadLine()) != null)
-        {
-            Console.Write ('\t'+line+'\n');           
-            if (Convert.ToInt32(line) > b & selectedfile)
+    public static void MergeFiles(string file0, string file1, string file2)
+    {
+        int count = 0;
+        var sr1 = File.ReadLines(file1);
+        var sr2 = File.ReadLines(file2);
+        using StreamWriter wr = new(file0);      
+            while (Math.Min(sr1.LongCount(), sr2.LongCount()) > count)
             {
-                stream1.WriteLine(line, true); ;
-                b = Convert.ToInt32(line);
-                Console.Write("f1:" +line); 
+                int fnumber1 = Convert.ToInt32(sr1.ElementAt(count));
+                int fnumber2 = Convert.ToInt32(sr2.ElementAt(count));
+                if (fnumber1 > fnumber2)
+                {
+                    wr.WriteLine(fnumber2);
+                    wr.WriteLine(fnumber1);
+                }
+                else
+                {
+                    wr.WriteLine(fnumber1);
+                    wr.WriteLine(fnumber2);
+                }
+                count++;
             }
-            else           
-            if (Convert.ToInt32(line) > b & selectedfile)
-            {
-                stream2.WriteLine(line, true);
-                b = Convert.ToInt32(line);
-                Console.Write(' ' + "f2:" +line);
-            }
-            else
-            {
-                switch (selectedfile)
-                { 
-                    case true:
-                        stream2.WriteLine(line, true);
-                        Console.Write(' ' + "f2:" + line);
-                        break;
-                    case false:
-                        stream1.WriteLine(line, true);
-                        Console.Write("f1:" + line);
-                        break;
-                }                
-                selectedfile=!selectedfile;
-                b = int.MinValue;
-            }
-        }     
-        stream0.Close();
-        stream1.Close();
-        stream2.Close();
-        stream0.Dispose();
-        stream1.Dispose();      
-        stream2.Dispose();  
+            if (sr1.LongCount() > sr2.LongCount())
+                while (count < sr1.LongCount())
+                {
+                    wr.WriteLine(Convert.ToInt32(sr1.ElementAt(count)));
+                    count++;
+                }
+            if (sr2.LongCount() > sr1.LongCount())
+                while (count < sr2.LongCount())
+                {
+                    wr.WriteLine(Convert.ToInt32(sr2.ElementAt(count)));
+                    count++;
+                }       
     }
 
+    public static void MergeSort(string file0, string file1, string file2)
+    {
+        DistributeFiles(file0, file1, file2);
+        MergeFiles(file0, file1, file2);
+        while (!IsTextFileEmpty(file1) & !IsTextFileEmpty(file2))
+        {
+            DistributeFiles(file0, file1, file2);
+            MergeFiles(file0, file1, file2);
+        }
+    }
+
+    public static bool IsTextFileEmpty(string filePath)
+    {
+        var info = new FileInfo(filePath);
+        if (info.Length == 0)
+            return true;
+        else return false;   
+    }
 }
+
+  
     
 
         
 
         
+
 
